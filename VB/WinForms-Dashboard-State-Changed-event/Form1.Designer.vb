3c35931f4786f948591d5dd5267a0b2c7c89b8ab
DevExpressExampleBot <fakeEmail>
2021-10-14 17:38:20 +0300

    autogenerate VB

--- VB/WinForms-Dashboard-State-Changed-event/Form1.Designer.vb
+++ VB/WinForms-Dashboard-State-Changed-event/Form1.Designer.vb
@@ -1,66 +1,67 @@
-﻿Namespace WinForms_Dashboard_State_Changed_event
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WinForms_Dashboard_State_Changed_event
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardViewer
-			' 
-			Me.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt((CByte(160)))), (CInt((CByte(160)))), (CInt((CByte(160)))))
-			Me.dashboardViewer.Appearance.Options.UseBackColor = True
-			Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer.Name = "dashboardViewer"
-			Me.dashboardViewer.Size = New System.Drawing.Size(919, 482)
-			Me.dashboardViewer.TabIndex = 0
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.dashboardViewer.DashboardLoaded += new DevExpress.DashboardWin.DashboardLoadedEventHandler(this.DashboardViewer_DashboardLoaded);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.dashboardViewer.SetInitialDashboardState += new DevExpress.DashboardWin.SetInitialDashboardStateEventHandler(this.DashboardViewer_SetInitialDashboardState);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.dashboardViewer.DashboardStateChanged += new DevExpress.DashboardWin.DashboardStateChangedEventHandler(this.DashboardViewer_DashboardStateChanged);
-			' 
-			' Form1
-			' 
-			Me.ClientSize = New System.Drawing.Size(919, 482)
-			Me.Controls.Add(Me.dashboardViewer)
-			Me.Name = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
-		Private dashboardBarAndDockingController1 As DevExpress.DashboardWin.Native.DashboardBarAndDockingController
-		Private dashboardBarController1 As DevExpress.DashboardWin.Bars.DashboardBarController
-		Private textBoxEditorBarController1 As DevExpress.DashboardWin.Bars.TextBoxEditorBarController
-		Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
-		Private WithEvents dashboardViewer As DevExpress.DashboardWin.DashboardViewer
-	End Class
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardViewer
+            ' 
+            Me.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((160)))))), (CInt(((CByte((160)))))), (CInt(((CByte((160)))))))
+            Me.dashboardViewer.Appearance.Options.UseBackColor = True
+            Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer.Name = "dashboardViewer"
+            Me.dashboardViewer.Size = New System.Drawing.Size(919, 482)
+            Me.dashboardViewer.TabIndex = 0
+            Me.dashboardViewer.DashboardLoaded += New DevExpress.DashboardWin.DashboardLoadedEventHandler(AddressOf Me.DashboardViewer_DashboardLoaded)
+            Me.dashboardViewer.SetInitialDashboardState += New DevExpress.DashboardWin.SetInitialDashboardStateEventHandler(AddressOf Me.DashboardViewer_SetInitialDashboardState)
+            Me.dashboardViewer.DashboardStateChanged += New DevExpress.DashboardWin.DashboardStateChangedEventHandler(AddressOf Me.DashboardViewer_DashboardStateChanged)
+            ' 
+            ' Form1
+            ' 
+            Me.ClientSize = New System.Drawing.Size(919, 482)
+            Me.Controls.Add(Me.dashboardViewer)
+            Me.Name = "Form1"
+            AddHandler Me.FormClosing, New System.Windows.Forms.FormClosingEventHandler(AddressOf Me.Form1_FormClosing)
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+#End Region
+        Private dashboardBarAndDockingController1 As DevExpress.DashboardWin.Native.DashboardBarAndDockingController
+
+        Private dashboardBarController1 As DevExpress.DashboardWin.Bars.DashboardBarController
+
+        Private textBoxEditorBarController1 As DevExpress.DashboardWin.Bars.TextBoxEditorBarController
+
+        Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
+
+        Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
+    End Class
 End Namespace
--- VB/WinForms-Dashboard-State-Changed-event/Form1.vb
+++ VB/WinForms-Dashboard-State-Changed-event/Form1.vb
@@ -1,40 +1,47 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWin
 Imports System.Windows.Forms
 Imports System.Xml.Linq
 
 Namespace WinForms_Dashboard_State_Changed_event
-	Partial Public Class Form1
-		Inherits Form
-		Public Shared ReadOnly PropertyName As String = "DashboardState"
-		Private state As New DashboardState()
-		Private Const path As String = "..\..\Dashboards\dashboard1.xml"
-		Public Sub New()
-			InitializeComponent()
-			dashboardViewer.LoadDashboard(path)
-		End Sub
 
-		Private Sub DashboardViewer_DashboardStateChanged(ByVal sender As Object, ByVal e As DashboardStateChangedEventArgs) Handles dashboardViewer.DashboardStateChanged
-			state = dashboardViewer.GetDashboardState()
-			Dim stateValue = state.SaveToXml().ToString(SaveOptions.DisableFormatting)
-			dashboardViewer.Dashboard.CustomProperties.SetValue("DashboardState", stateValue)
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub DashboardViewer_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs) Handles dashboardViewer.SetInitialDashboardState
-			e.InitialState = state
-		End Sub
+        Private state As DashboardState = New DashboardState()
 
-		Private Sub GetDataFromString(ByVal customPropertyValue As String)
-			If (Not String.IsNullOrEmpty(customPropertyValue)) Then
-				Dim xmlStateEl = XDocument.Parse(customPropertyValue)
-				state.LoadFromXml(xmlStateEl)
-			End If
-		End Sub
-		Private Sub DashboardViewer_DashboardLoaded(ByVal sender As Object, ByVal e As DashboardLoadedEventArgs) Handles dashboardViewer.DashboardLoaded
-			GetDataFromString(dashboardViewer.Dashboard.CustomProperties.GetValue(PropertyName))
-		End Sub
-		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
-			dashboardViewer.Dashboard.SaveToXml(path)
-		End Sub
-	End Class
+        Public Shared ReadOnly PropertyName As String = "DashboardState"
+
+        Const path As String = "..\..\Dashboards\dashboard1.xml"
+
+        Public Sub New()
+            InitializeComponent()
+            dashboardViewer.LoadDashboard(path)
+        End Sub
+
+        Private Sub DashboardViewer_DashboardStateChanged(ByVal sender As Object, ByVal e As DashboardStateChangedEventArgs)
+            state = dashboardViewer.GetDashboardState()
+            Dim stateValue = state.SaveToXml().ToString(SaveOptions.DisableFormatting)
+            dashboardViewer.Dashboard.CustomProperties.SetValue("DashboardState", stateValue)
+        End Sub
+
+        Private Sub DashboardViewer_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
+            e.InitialState = state
+        End Sub
+
+        Private Sub GetDataFromString(ByVal customPropertyValue As String)
+            If Not String.IsNullOrEmpty(customPropertyValue) Then
+                Dim xmlStateEl = XDocument.Parse(customPropertyValue)
+                state.LoadFromXml(xmlStateEl)
+            End If
+        End Sub
+
+        Private Sub DashboardViewer_DashboardLoaded(ByVal sender As Object, ByVal e As DashboardLoadedEventArgs)
+            GetDataFromString(dashboardViewer.Dashboard.CustomProperties.GetValue(PropertyName))
+        End Sub
+
+        Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
+            dashboardViewer.Dashboard.SaveToXml(path)
+        End Sub
+    End Class
 End Namespace
